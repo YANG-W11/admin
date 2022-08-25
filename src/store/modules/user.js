71c1a00c09bc7a29a8b0707e65536b85cb998076
YANG-W <1451392405@qq.com>
2022-08-25 19:49:48 +0800

    token持久化配置以及axios相关配置

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,8 +1,24 @@
-
+import { getToken, setToken, removToken } from '@/utils/auth'
+// 状态
+const state = {
+  token: getToken()
+}
+const mutations = {
+  setToken(state, token) {
+    state.token = token
+    setToken(token)
+  },
+  // 删除token
+  removToken(state) {
+    state.token = null
+    removToken()
+  }
+}
+const actions = {}
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state,
+  mutations,
+  actions
 }
 
